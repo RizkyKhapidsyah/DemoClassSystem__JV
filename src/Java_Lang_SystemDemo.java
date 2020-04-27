16298e9c9643246ff72258dea28bdd7d07afe5ee
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-04-28 00:07:57 +0700

    Initial commit

---
+++ src/Java_Lang_SystemDemo.java
@@ -0,0 +1,36 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Java_Lang_SystemDemo {
+    public Java_Lang_SystemDemo() {
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        int arr1[] = new int[1050000];
+        int arr2[] = new int[1050000];
+        long startTime, endTime;
+
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = i + 1;
+        }
+
+        startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < arr1.length; i++) {
+            arr2[i] = arr1[i];
+        }
+
+        endTime = System.currentTimeMillis();
+        System.out.println("Time for manual copy: " + (endTime - startTime) + " ms.");
+
+        startTime = System.currentTimeMillis();
+        System.arraycopy(arr1, 0, arr2, 0, arr1.length);
+
+        endTime = System.currentTimeMillis();
+        System.out.println("Time for manual copy: " + (endTime - startTime) + " ms.");
+        System.gc(); //force garbage collector
+        System.setIn(new FileInputStream("temp.txt"));
+        System.exit(0);
+    }
+}
